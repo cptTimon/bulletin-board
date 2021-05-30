efd50c38e1187390128323c75c4506d4ee6b5580
Wojciech Kaczmarzyński <wojciech.kaczmarzynski@gmail.com>
2021-05-30 15:40:15 +0200

    Adding changing user

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -2,28 +2,50 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import clsx from 'clsx';
+
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import { UserNav } from '../UserNav/UserNav';
 import { NoUserNav } from '../NoUserNav/NoUserNav';
-// import { connect } from 'react-redux';
-// import { reduxSelector, reduxActionCreator } from '../../../redux/exampleRedux.js';
+import Link from '@material-ui/core/Link';
+import { Link as RouterLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { getUser, login, logout } from '../../../redux/userRedux.js';
 
 import styles from './Header.module.scss';
 
-const Component = ({className, children}) => {
-  const isLogged = true;
-  const Nav = isLogged ? UserNav : NoUserNav;
+const Component = ({className, user, login, logout}) => {
+  const Nav = user ? UserNav : NoUserNav;
+
+  const selectUser = ({target}) => {
+    if (target.value) {
+      login({type: target.value});
+    } else logout();
+  };
 
   return (
     <div className={clsx(className, styles.root)}>
       <AppBar position="fixed">
         <Toolbar className={styles.toolbar}>
-          <Typography variant="h6" component="h1" className={styles.title}>
-          Bulletin Board
-          </Typography>
+          <Link
+            component={RouterLink}
+            to='/'
+            variant="h6"
+            className={styles.title}
+            color='inherit'
+            underline='none'
+          >
+            Bulletin Board
+          </Link>
           <Nav className={styles.nav}/>
+          <select
+            value={user ? user.type : ''}
+            onChange={selectUser}
+          >
+            <option value=''>No user</option>
+            <option value='regUser'>User</option>
+            <option value='admin'>Admin</option>
+          </select>
         </Toolbar>
       </AppBar>
       <Toolbar />
@@ -32,22 +54,25 @@ const Component = ({className, children}) => {
 };
 
 Component.propTypes = {
-  children: PropTypes.node,
   className: PropTypes.string,
+  user: PropTypes.object,
+  login: PropTypes.func,
+  logout: PropTypes.func,
 };
 
-// const mapStateToProps = state => ({
-//   someProp: reduxSelector(state),
-// });
+const mapStateToProps = state => ({
+  user: getUser(state),
+});
 
-// const mapDispatchToProps = dispatch => ({
-//   someAction: arg => dispatch(reduxActionCreator(arg)),
-// });
+const mapDispatchToProps = dispatch => ({
+  login: arg => dispatch(login(arg)),
+  logout: () => dispatch(logout()),
+});
 
-// const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
+const Container = connect(mapStateToProps, mapDispatchToProps)(Component);
 
 export {
-  Component as Header,
-  // Container as Header,
+  // Component as Header,
+  Container as Header,
   Component as HeaderComponent,
 };
